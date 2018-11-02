4cf8515d3f6b2dba600499e0ab37e4018845773d
James <jknight@lunenfeld.ca>
2018-11-02 16:06:48 -0400

    Update setup/socket-config.js

--- setup/socket-config.js
+++ setup/socket-config.js
@@ -2,7 +2,7 @@ const socketIo = require('socket.io');
 
 const socketConfig = (app, server) => {
   // Set up socket.
-  const io = socketIo(server);
+  const io = socketIo(server, { path: '/ws' });
   io.on('connection', (client) => {
     client.emit('action', { id: client.id, type: 'SET_SESSION_ID' });
   });
