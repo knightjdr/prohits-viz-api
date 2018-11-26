ea385a56cb85816feacc304bebc94217bbe63adc
James <jknight@lunenfeld.ca>
2018-11-26 16:04:17 -0500

    Update cache-route.js

--- app/middleware/cache-route.js
+++ app/middleware/cache-route.js
@@ -5,15 +5,14 @@ const CACHE_TIME = 15; // Cache time in minutes;
 const cache = (req, res, next) => {
   const key = `__express__${req.originalUrl || req.url}`;
   const cachedBody = mcache.get(key);
-  const cachceMS = CACHE_TIME * 60000;
-  res.setHeader('Cache-Control', `public, max-age=${cachceMS}`);
-  res.setHeader('Expires', new Date(Date.now() + cachceMS).toUTCString());
+  const cacheMS = CACHE_TIME * 60000;
+  res.setHeader('Expires', new Date(Date.now() + cacheMS).toUTCString());
   if (cachedBody) {
     res.send(cachedBody);
   } else {
     res.sendResponse = res.send;
     res.send = (body) => {
-      mcache.put(key, body, cachceMS);
+      mcache.put(key, body, cacheMS);
       res.sendResponse(body);
     };
     next();
