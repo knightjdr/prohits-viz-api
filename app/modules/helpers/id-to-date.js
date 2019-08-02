6898991f447a793555c28de65d4c1f96c5888559
James <jknight@lunenfeld.ca>
2019-08-02 15:33:17 -0400

    Modify mongo ID date conversion to handle string IDs

--- app/modules/helpers/id-to-date.js
+++ app/modules/helpers/id-to-date.js
@@ -3,6 +3,9 @@ const { ObjectID } = require('mongodb');
 // grabs the ISO date from a mongo _id field
 const idToDate = (id) => {
   if (ObjectID.isValid(id)) {
+    if (typeof id === 'string') {
+      return ObjectID(id).getTimestamp();
+    }
     return id.getTimestamp();
   }
   return null;
--- app/modules/helpers/id-to-date.test.js
+++ app/modules/helpers/id-to-date.test.js
@@ -1,15 +1,19 @@
 const idToDate = require('./id-to-date');
 const { ObjectID } = require('mongodb');
 
-const id = ObjectID('5aa6ac91c63eb43ab21a072c');
-const invalidId = '5aa6ac91c63eb43ab21a072';
 
 describe('convert mongoDB _id to date', () => {
-  it('should return null when date is invalide', () => {
-    expect(idToDate(invalidId)).toBeNull();
+  it('should return null when date is invalid', () => {
+    expect(idToDate('5aa6ac91c63eb43ab21a072')).toBeNull();
   });
 
   it('return timestamp when id is valid', () => {
+    const id = ObjectID('5aa6ac91c63eb43ab21a072c');
     expect(idToDate(id)).toEqual(id.getTimestamp());
+  });
+
+  it('return timestamp when id is valid and a string', () => {
+    const id = ObjectID('5aa6ac91c63eb43ab21a072c');
+    expect(idToDate('5aa6ac91c63eb43ab21a072c')).toEqual(id.getTimestamp());
   });
 });
