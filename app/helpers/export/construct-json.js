711ccc4648219dbd9e6961bc39206c4ba04367d3
James Knight <knightjdr@gmail.com>
2020-02-14 21:43:21 -0500

    Add markup fields to export json

--- app/helpers/export/construct-json.js
+++ app/helpers/export/construct-json.js
@@ -1,7 +1,9 @@
 const constructJSON = data => ({
+  annotations: data.annotations,
   columnDB: data.columnDB,
   columnOrder: data.columnOrder,
   imageType: data.imageType,
+  markers: data.markers,
   rowDB: data.rowDB,
   rowOrder: data.rowOrder,
   settings: {
--- app/helpers/export/construct-json.test.js
+++ app/helpers/export/construct-json.test.js
@@ -3,9 +3,28 @@ import constructJson from './construct-json.js';
 describe('Construct JSON for pvsync', () => {
   it('should convert object to required format', () => {
     const data = {
+      annotations: {
+        fontSize: 14,
+        list: {
+          id1: {
+            text: 'a',
+            position: { x: 0.1, y: 0.2 },
+          },
+        },
+      },
       columnDB: ['a', 'b', 'c'],
       columnOrder: [0, 1, 2],
       imageType: 'dotplot',
+      markers: {
+        list: {
+          id1: {
+            height: 10,
+            width: 15,
+            x: 0.5,
+            y: 0.2,
+          },
+        },
+      },
       rowDB: [{ data: [{ ratio: 0.5, score: 0.01, value: 1 }], name: 'a' }],
       rowOrder: [0],
       settings: {
@@ -20,9 +39,11 @@ describe('Construct JSON for pvsync', () => {
     };
 
     const expected = {
+      annotations: data.annotations,
       columnDB: data.columnDB,
       columnOrder: data.columnOrder,
       imageType: data.imageType,
+      markers: data.markers,
       rowDB: data.rowDB,
       rowOrder: data.rowOrder,
       settings: {
