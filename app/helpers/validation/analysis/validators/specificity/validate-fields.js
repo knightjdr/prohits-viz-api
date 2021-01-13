24f2dc837a2ef004b27c2af3a8e3580111ad1ea1
James Knight <knightjdr@gmail.com>
2021-01-13 15:31:59 -0500

    Remove minConditions from specificity form validation

--- app/helpers/validation/analysis/validators/specificity/validate-fields.js
+++ app/helpers/validation/analysis/validators/specificity/validate-fields.js
@@ -1,6 +1,5 @@
 import criteria from '../../field-validation/criteria.js';
 import validateCommon, { getFieldValidator } from '../common/validate-fields.js';
-import validateMinCondition from '../../field-validation/min-conditions.js';
 
 const acceptedMetrics = ['fe', 'zscore', 'sscore', 'dscore', 'wdscore'];
 
@@ -20,8 +19,6 @@ const validateFields = (type, value) => {
     switch (type) {
       case 'minAbundance':
         return validateField(criteria.isNumber, 'should be a number');
-      case 'minConditions':
-        return validateField(validateMinCondition, 'should be a number > 0');
       case 'primaryFilter':
         return validateField(criteria.isNumber, 'should be a number');
       case 'specificityMetric':
