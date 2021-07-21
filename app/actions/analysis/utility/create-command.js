9f06106b39447ad0ffd8cd0c36c951e53b522ab7
James Knight <knightjdr@gmail.com>
2021-07-21 14:10:12 -0400

    Update utility docker command

--- app/actions/analysis/utility/create-command.js
+++ app/actions/analysis/utility/create-command.js
@@ -1,11 +1,11 @@
 const scripts = {
   pvconvert: fields => `pvconvert --file="${fields.files[0]}" --imageType="${fields.imageType}"`,
-  saint_domain_enrich: fields => 'docker run -v $(pwd):/files/ pvutilitiespython /app/saint_domain_enrich.py '
+  saint_domain_enrich: fields => 'docker run --rm -v $(pwd):/files/ pvutilitiespython /app/saint_domain_enrich.py '
     + `-b ${fields.background} -f ${fields.fdr} -i ${fields.idType} -s ${fields.files[0]} -t ${fields.topPreys} `
     + `-d ${fields.domainFile} -g ${fields.geneFile}`,
-  saint_fea: fields => 'docker run -v $(pwd):/files/ pvutilitiespython /app/saint_fea.py '
+  saint_fea: fields => 'docker run --rm -v $(pwd):/files/ pvutilitiespython /app/saint_fea.py '
     + `-f ${fields.fdr} -s ${fields.files[0]} -t ${fields.topPreys}`,
-  saint_stats: fields => 'docker run -v $(pwd):/files/ pvutilitiespython /app/saint_stats.py '
+  saint_stats: fields => 'docker run --rm -v $(pwd):/files/ pvutilitiespython /app/saint_stats.py '
     + `-f ${fields.fdr} -s ${fields.files[0]}`,
 };
 
