c39095dda0921b60eb37ad1e59509e4cc3a6049f
James Knight <knightjdr@gmail.com>
2022-03-11 13:43:44 -0500

    Add health check

---
+++ app/actions/health/health.js
@@ -0,0 +1,5 @@
+const checkHealth = (req, res) => {
+  res.status(200).send({ message: 'OK' });
+};
+
+export default checkHealth;
--- app/routes/routes-get.js
+++ app/routes/routes-get.js
@@ -2,6 +2,7 @@ import cacheAPI from './middleware/cache-route.js';
 import messages from './route-messages.js';
 import noCacheClient from './middleware/no-cache.js';
 
+import checkHealth from '../actions/health/health.js';
 import downloadDataFile from '../actions/file/download-data-file.js';
 import downloadFile from '../actions/file/download-file.js';
 import downloadFolder from '../actions/task/download/download-folder.js';
@@ -14,6 +15,7 @@ import updateStatus from '../actions/task/status/update-status.js';
 const get = (router) => {
   router.get('/data/:file', downloadDataFile);
   router.get('/file/:file(*)', downloadFile);
+  router.get('/health', checkHealth);
   router.get('/home/', noCacheClient, cacheAPI, getHomeContent);
   router.get('/news/', noCacheClient, cacheAPI, getNewsArticles);
   router.get('/news/:headline', noCacheClient, cacheAPI, getArticle);
